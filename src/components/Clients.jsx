71113e97ab60bdff4cf5277a50510997c8446f79
pblgllgs <pbl.gllgs@gmail.com>
2022-09-22 19:08:55 -0300

    clients, title, and others components

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { clients } from '../constants';
+import styles from '../styles';
 
 const Clients = () => {
   return (
-    <div>Clients</div>
-  )
-}
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => {
+          return (
+            <div
+              key={client.id}
+              className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`}
+            >
+              <img
+                src={client.logo}
+                alt="client"
+                className="sm:w-[192px] w-[100px] object-contain hover:brightness-200"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Clients
+export default Clients;
